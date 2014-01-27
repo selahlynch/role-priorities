e05e11e9fb3e2005657fe4de508966f8f5f72f33
Selah Lynch <selahlynch@gmail.com>
2014-01-27 10:50:39 -0500

    make role mappings script easier to use!

--- makeRoleMappings.py
+++ makeRoleMappings.py
@@ -2,28 +2,28 @@ import json
 
 #manually make roles.json out of roles.js
 
-#take a roles.json file and pull out the company names
-#for each company name create two mappings of xxxx_1=>xxxx and xxxx_2=>xxxx
-
-
 rolesfile = open('roles.json')
 roles = json.load(rolesfile)
+
+
+#FOUNDERS
 role_mappings = {}
+company_names = [company["name"] for company in roles["companies"]]
+for name in company_names:
+    role_mappings[name+"_1"] = name
+    role_mappings[name+"_2"] = name
+    role_mappings[name+"_3"] = name
+print json.dumps(role_mappings, indent=4, sort_keys=True);
 
-#company_names = [company["name"] for company in roles["companies"]]
-#for name in company_names:
-#    role_mappings[name+"_1"] = name
-#    role_mappings[name+"_2"] = name
-#    role_mappings[name+"_3"] = name
 
+#INVESTORS
+role_mappings = {}
 investor_names = [investor["name"] for investor in roles["investors"]]
 for name in investor_names:
     role_mappings[name+"_1"] = name
     role_mappings[name+"_2"] = name
-
-
-#print json.dumps(company_names, indent=4);
 print json.dumps(role_mappings, indent=4, sort_keys=True);
+
 
 
 
